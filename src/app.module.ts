e52d0a2df2472924cc9a49a47bfb6bbcca1c11af
이샘미 <isaemmi@isaemmiui-MacBookPro.local>
2022-07-05 20:24:45 +0900

    dabae pj

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CourseModule } from './course/course.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '10.53.192.10',
+      port: 3306,
+      username: 'root',
+      password: '12345',
+      database: 'dabaeDB',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: './src/commons/graphql/schema.gql',
